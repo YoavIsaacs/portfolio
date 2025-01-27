14239c677e3578fd83db3c2d7422146c2e6fb3ea
Yoav Isaacs <yoav.isaacs@gmail.com>
2025-01-27 21:57:32 +0200

    created intro section

---
+++ src/components/MainSection/IntroductionSection/IntroductionSection.tsx
@@ -0,0 +1,13 @@
+
+
+const IntroductionSection = () => {
+  return (
+    <div className="px-48 pt-52 w-5/6 text-text-default size-1/2 p-6">
+      <h1 className="text-8xl mb-4">Hello, I'm Yoav Isaacs.</h1>
+      <h2 className="text-6xl">I'm a software developer, nice to meet you.</h2>
+    </div>
+
+  );
+}
+
+export default IntroductionSection;
